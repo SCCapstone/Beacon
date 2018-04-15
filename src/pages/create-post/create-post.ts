6a37272dfd315a32d1225e80125451154016cf7d
Amanda Brummett <amandamb@email.sc.edu>
2018-04-15 18:45:24 -0400

    added missing comma on create-post.ts

--- src/pages/create-post/create-post.ts
+++ src/pages/create-post/create-post.ts
@@ -155,7 +155,7 @@ userEmail: Observable<any>;
     organization: this.organization,  
    
     ppURL: this.ppURL,  //profile picture url
-    postImgURL: this.postImgURL   //post image url
+    postImgURL: this.postImgURL,   //post image url
 
    // username: this.name
    latitude: parseFloat(this.latitude),
